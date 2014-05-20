c9f935a95dc79ea255e790db1afa68bad49bafdc
y12studio <y12studio@gmail.com>
2014-05-20 18:44:32 +0800

    init composer

---
+++ web/index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>Dokku Template PHP Project</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World dok-php-hello</p>'; ?>
+ <a href="info.php">phpinfo</a>
+ </body>
+</html>
