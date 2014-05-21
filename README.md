bdffb96a15372e55d0fd1a5c05c092f1fdd48eea
y12studio <y12studio@gmail.com>
2014-05-21 17:28:01 +0800

    move to CDN. fix #3

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ dok-php-hello
 =============
 
 ```
-git remote add y12test dokku@YOURHOSTNAME:YOURAPPNAME
-git push y12test master
+git clone https://github.com/y12studio/dok-php-hello /var/www/html/foo/dok-php-hello
+cd /var/www/html/foo/dok-php-hello && rm -rf .git
+nano web/xxx.php
+git remote add dokku dokku@HOSTNAME:APPNAME
+git push dokku master
 ```
--- web/index.php
+++ web/index.php
@@ -11,19 +11,12 @@
     <title>Dokku PHP/bootstrap/jquery</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="http://cdn.bootcss.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="http://cdn.bootcss.com/bootstrap/3.1.1/css/bootstrap-theme.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
@@ -56,8 +49,8 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1><?php echo 'Hello World dok-php-hello'; ?></h1>
-        <p>php info link</p>
+        <h1><span class="blue glyphicon glyphicon-cloud"></span><?= 'dok-php-hello'?></h1>
+        <p>php info link <span class="glyphicon glyphicon-heart red"></span></p>
         <p><a class="btn btn-primary btn-lg" role="button" href="info.php">phpinfo &raquo;</a></p>
       </div>
     </div>
@@ -99,8 +92,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+	<script src="http://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+	<script src="http://cdn.bootcss.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
   </body>
 </html>
 
